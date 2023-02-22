5f5b42743a09e5916913c9afd57576588f24069d
Kaden Franklin <kadenfranklin@hotmail.com>
2023-02-22 10:02:11 -0600

    Made own scene, character, walking + jumping animations

---
+++ CSCIprojectThree/Assets/Scripts/Walking.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour
+{
+
+    private Rigidbody2D body;
+    public float horizontal;
+
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    private bool jumping = false;
+
+    public float runSpeed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("horizontal", horizontal);
+        if (horizontal < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            spriteRenderer.flipX = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && !jumping)
+        {
+            print("Space!");
+            body.AddForce(new Vector2(0, 400));
+            jumping = true;
+            animator.SetBool("jumping", jumping);
+
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+        body.velocity = new Vector2(horizontal * runSpeed, body.velocity.y);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision2D)
+    {
+        jumping = false;
+        animator.SetBool("jumping", jumping);
+
+    }
+}
